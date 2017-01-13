fcb29a6325644f4d091d0c7c53790d8c61f4fec9
Asonna <Asonna>
2017-01-13 10:53:32 -0800

    add first model around question and associated components

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  question: "What does the Router do with the information from the URL?",
+  author: "Billy Jones",
+  Additional notes: "Ember in Javascript"
+}, {
+  id: 2,
+  question: "What is the Route Handler responsible for? What 'triggers' it into action, and what action does it then perform?",
+  author: "Ray Hamilton",
+  Additional notes: "Ember in Javascript"
+}, {
+  id: 3,
+  question: "What portion of the application is responsible for accessing the model? When and where does this occur?",
+  author: "Anne Moretti",
+  Additional notes: "Ember in Javascript"
+  // answers:
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
