a935a8e36ec4d8f6af12dbbaaab7de575911911c
Asonna <Asonna>
2017-01-13 12:02:50 -0800

    redirect the application to now grabing content from database through firebase. Import data to firebase db

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,25 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  question: "What does the Router do with the information from the URL?",
-  author: "Billy Jones",
-  notes: "Javascript"
-}, {
-  id: 2,
-  question: "What is the Route Handler responsible for? What triggers it into action, and what action does it then perform?",
-  author: "Ray Hamilton",
-  notes: "Ember/Javascript"
-}, {
-  id: 3,
-  question: "What portion of the application is responsible for accessing the model? When and where does this occur?",
-  author: "Anne Moretti",
-  notes: "Javascript"
-  // answers:
-}];
+// var questions = [{
+//   id: 1,
+//   question: "What does the Router do with the information from the URL?",
+//   author: "Billy Jones",
+//   notes: "Javascript"
+// }, {
+//   id: 2,
+//   question: "What is the Route Handler responsible for? What triggers it into action, and what action does it then perform?",
+//   author: "Ray Hamilton",
+//   notes: "Ember/Javascript"
+// }, {
+//   id: 3,
+//   question: "What portion of the application is responsible for accessing the model? When and where does this occur?",
+//   author: "Anne Moretti",
+//   notes: "Javascript"
+//   // answers:
+// }];
 
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });
